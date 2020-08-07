27c31b90e841b371ea1513430939a8bb64bc3b86
kaiovbraga <kaiovbraga2001@gmail.com>
2020-08-06 23:05:11 -0300

    Implementaçao da Tela de login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 import Goal from './pages/Goal';
 import Habit from './pages/Habit';
 
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <Router>
       <Route path="/signup" component={SignUp} />
+      <Route path="/login" component={Login} />
       <Route path="/goal"  component={Goal} />
       <Route path="/habit"  component={Habit} />
     </Router>
---
+++ src/pages/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <h1>Login</h1>
+
+        <div style={{ 
+          display: 'flex', 
+          flexDirection: 'column',
+          width: '40vw' 
+        }}>
+          <input 
+            placeholder="Seu email"
+            value={email}
+            onChange={ e => setEmail(e.target.value)}
+            style={{
+              marginBottom: '20px'
+            }}
+            type="email" 
+          />
+          <input
+            placeholder="Sua senha"
+            value={password}
+            onChange={ e => setPassword(e.target.value)}
+            style={{
+              marginBottom: '20px'
+            }} 
+            type="password" 
+          />
+
+          <button 
+            style={{ 
+              backgroundColor: "#646abd",
+              paddingTop: '10px',
+              paddingBottom: '10px',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              fontSize: '15px'
+            }}
+          >
+            Cadastrar
+          </button>
+        </div>
+    </div>
+  );
+}
+
+export default SignUp;
