879994005f0ae7809a6b54e6c48155e7dc702491
kaiovbraga <kaiovbraga2001@gmail.com>
2021-03-23 23:53:25 -0300

    Usando api local

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
-import https from 'https';
-
-export const api = axios.create({
-    baseURL: 'https://kaio-giulia-habit-tracker-api.herokuapp.com/',
-    httpsAgent: new https.Agent({
-        rejectUnauthorized: false
-    })
-})
+import axios from "axios";
+import https from "https";
 
 // export const api = axios.create({
-//     baseURL: 'http://localhost:3333/',
+//     baseURL: 'https://kaio-giulia-habit-tracker-api.herokuapp.com/',
 //     httpsAgent: new https.Agent({
 //         rejectUnauthorized: false
 //     })
 // })
+
+export const api = axios.create({
+  baseURL: "http://localhost:3333/",
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
+});
 
 // export const local = axios.create({
 //     baseURL: 'http://localhost:3333/',
