497398ced30d83d34af4f67958f7dd94c342199c
kaiovbraga <kaiovbraga2001@gmail.com>
2021-04-04 23:29:50 -0300

    Ajuste no posicionamento interno do statistics

--- src/components/Statistics/styles.ts
+++ src/components/Statistics/styles.ts
@@ -1,61 +1,63 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 60rem;
-    height: 100%;
-    margin-bottom: 10vh;
-    background-color: #cecece;
-    display: flex;
-    flex-direction: column;
+  width: 60rem;
+  height: 100%;
+  margin-bottom: 10rem;
+  background-color: #cecece;
+  display: flex;
+  flex-direction: column;
 
-    & > div:not(:last-of-type) {
-        margin-right: 1.5rem;
-    }
+  justify-content: center;
+
+  & > div:not(:last-of-type) {
+    margin-right: 1.5rem;
+  }
 `;
 
 export const DescriptionContainer = styled.table`
-    width: 60rem;
-    height: 100%;
-    padding-top: 10vh;
-    
-    /* table-layout: fixed; */
+  width: 60rem;
+  height: 100%;
+  padding: 5rem 0;
 
-    & > tr {
-        width: 100%;
-        line-height: 1.8;
+  /* table-layout: fixed; */
 
-        & > td:first-child {
-            min-width: max-content;
-            width: 65%;
-            padding: 0 10px;
-        }
+  & > tr {
+    width: 100%;
+    line-height: 1.8;
 
-        & > td {
-            width: 35%;
-            padding: 0 10px;
-        }
+    & > td:first-child {
+      min-width: max-content;
+      width: 65%;
+      padding: 0 10px;
     }
+
+    & > td {
+      width: 35%;
+      padding: 0 10px;
+    }
+  }
 `;
 
 export const BarsContainer = styled.div`
-    width: 60rem;
-    height: 100%;
-    margin-bottom: 10vh;
-    display: flex;
-    flex-direction: row;
-    align-items: flex-end;
+  width: 60rem;
+  height: 100%;
+  margin-bottom: 10vh;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
 
-    & > div:not(:last-of-type) {
-        margin-right: 1.5rem;
-    }
+  & > div:not(:last-of-type) {
+    margin-right: 1.5rem;
+  }
 `;
 
 interface BarProps {
-    percentage: number;
+  percentage: number;
 }
 
 export const Bar = styled.div<BarProps>`
-    width: 100%;
-    height: ${ props => props.percentage / 100 * 50}rem;
-    background-color: #6868ce;
+  width: 100%;
+  height: ${(props) => (props.percentage / 100) * 50}rem;
+  background-color: #6868ce;
 `;
